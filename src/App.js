0a9a6e83b67f2fccde9254a8429b116257996e7b
rohitazad <ram9953878727@gmail.com>
2023-05-09 16:37:58 +0530

    Default value and nested object do in react hook form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import UserInfoValidationForm from './Components/UserinfoValidationForm';
+import UserInfoValidationForm from './Components/UserinfoDefaultValue';
 function App() {
+  const _userData = {
+    name:'Rohit Azad Malik',
+    email:'rohitazadwebdesigner@gmail.com',
+    phone:9953878727
+  }
   return (
     <>
       <h1>Rohit Azad Malik (React js Form Validation with React Hook Form)</h1>
-      <UserInfoValidationForm />
+      <UserInfoValidationForm data={_userData} />
     </>
   );
 }
